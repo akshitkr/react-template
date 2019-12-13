e9d090a327c6b5ef6d957797e5ad45d14193920d
dotakshit <akshitkumar31102003@gmail.com>
2019-12-13 16:28:27 +0530

    new stuf

---
+++ src/components/Main.js
@@ -0,0 +1,138 @@
+import React from 'react';
+
+function Main(){
+    return(
+        <div className='boxcont'>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            <div className='box'>
+            Main lol
+            </div>
+            
+
+            
+            
+            
+        </div>
+    
+        
+    
+    )
+}
+
+export default Main
